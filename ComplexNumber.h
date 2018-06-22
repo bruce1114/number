5ee829307ed094e69f0b8f197eb4976f609d3275
bruce1114 <2821207842@qq.com>
2018-06-22 23:46:58 +0800

    updateagainandagain

--- ComplexNumber.h
+++ ComplexNumber.h
@@ -1,7 +1,7 @@
 #ifndef COMPLEXNUMBER_H
 #define COMPLEXNUMBER_H
-
 #include"Number.h"
+
 class ComplexNumber:public Number{
 private:
 	double real,virt;
@@ -9,14 +9,8 @@ public:
 	ComplexNumber();
 	ComplexNumber(double,double);
 	void print();
-<<<<<<< HEAD
 	ComplexNumber operator + (const ComplexNumber&);
 	ComplexNumber operator * (const ComplexNumber&);
-=======
-	ComplexNumber add(const ComplexNumber&);
-	ComplexNumber mul(const ComplexNumber&);
->>>>>>> 8e83ea9214ba7594532b2976b68eb9890e1c1a78
 };
-
 
 #endif
